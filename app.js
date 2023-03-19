5b5d67fbcb2202907e9b2a07c4e5b76014020e73
beistroff <galiasandrii@gmail.com>
2023-03-19 17:53:44 +0200

    I implemented interfaces for comments

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const http = require('http');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const commentsRoutes = require('./routes/comments');
+const requestDuration = require('./scripts/middleware/requestDuration');
 
 const app = express();
 
@@ -13,14 +16,42 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+app.use(express.json());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// const getDurationInMilliseconds = (start) => {
+//     const NS_PER_SEC = 1e9
+//     const NS_TO_MS = 1e6
+//     const diff = process.hrtime(start)
+
+//     return (diff[0] * NS_PER_SEC + diff[1]) / NS_TO_MS
+// }
+
+// app.use((req, res, next) => {
+//     console.log(`${req.method} ${req.originalUrl} [STARTED]`)
+//     const start = process.hrtime()
+
+//     res.on('finish', () => {            
+//         const durationInMilliseconds = getDurationInMilliseconds (start)
+//         console.log(`${req.method} ${req.originalUrl} [FINISHED] ${durationInMilliseconds.toLocaleString()} ms`)
+//     })
+
+//     res.on('close', () => {
+//         const durationInMilliseconds = getDurationInMilliseconds (start)
+//         console.log(`${req.method} ${req.originalUrl} [CLOSED] ${durationInMilliseconds.toLocaleString()} ms`)
+//     })
+
+//     next()
+// });
+
+app.use(requestDuration.duration);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use(commentsRoutes);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
---
+++ controllers/comments.controller.js
@@ -0,0 +1,45 @@
+// I use axios because it allows me to do an easy asynchronous HTTP request to REST API.
+const axios = require('axios');
+const url = 'http://gorest.co.in/public/v2/posts';
+
+const getComments = async (req, res) => {
+   try {
+    const postID = req.params.postID;
+    const response = await axios.get(`${url}/${postID}/comments`);
+    res.status(200).json(response.data);
+   } catch (error) {
+    console.error(error);
+    res.status(500).send('Server Error');
+   }
+};
+// here data it's our payload returned from the server 
+
+const createComment = async (req, res) => {
+  try {
+    const postID = req.params.postID;
+    const { id, post_id, name, email, body } = req.body;
+    const token = 'db2731ec124ce0fa02b868043f6c679bdb933f0219e9eea6ce8bc95e9dfe1053';
+
+    const headers = {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    };
+
+    const response = await axios.post(`${url}/${postID}/comments`, {
+        id: id,
+        post_id: post_id,
+        name: name, 
+        email: email,
+        body: body
+    }, { headers });
+    res.status(201).json(response.data.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server Error in POST request');
+  }
+};
+
+module.exports = {
+    getComments,
+    createComment
+};
