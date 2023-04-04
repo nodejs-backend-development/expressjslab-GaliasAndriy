c40b1af976b4b6e52fffef8ce6d2021978e8ba96
beistroff <galiasandrii@gmail.com>
2023-04-04 21:17:03 +0300

    I successfully implemented a unit tests (i hope)

--- tests/controllers/comments.test.js
+++ tests/controllers/comments.test.js
@@ -1,62 +1,89 @@
+const axios = require('axios');
+const request = require('supertest');
+const app = require('../../app');
+const keys = require('../../global_keys/keys');
+const { createCommentRequest } = require('../../clients/httpClient');
 require('dotenv').config();
 
-// Here will appears tests for my requests (comments)
-const axios = require('axios');
-const keys = require('../../global_keys/keys');
-
-const token = keys.TOKEN;
+jest.mock('axios');
 
 const url = keys.REST_API_URL;
 
 describe('GET /posts/:postID/comments', () => {
-    it('should return post for provided postID', async () => {
-        const postID = 1;
-        const res = await axios.get(`${url}/${postID}/comments`);
-        expect(res.status).toEqual(200);
-        expect(res.data).toBeInstanceOf(Array);
-        expect(Array.isArray(res.data)).toBe(true);
-        // expect(res.data.length).toBeGreaterThan(0); // failed, but it could be
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+    it('should return all comments under post', async () => {
+        const mockData = [
+            { 
+                id: 333, 
+                post_id: 1, 
+                name: "Test name", 
+                email: "testtest@outlook.com", 
+                body: "unit test" 
+            }
+        ];
+        axios.get.mockResolvedValue({ data: mockData });
+
+        const res = await request(app).get('/posts/1/comments');
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toEqual(mockData);
+        expect(axios.get).toHaveBeenCalledWith(`${url}/1/comments`);
+        expect(axios.get).toHaveBeenCalledTimes(1);
     });
 
-    it('should handle errors', async () => {
-        const postID = 'invalid';
-        try {
-            await axios.get(`${url}/${postID}/comments`);
-        } catch (error) {
-            expect(error.res.status).toEqual(500);
-            expect(error.res.data).toEqual('Server Error');
-        }
+    it('should return error on server error', async () => {
+        axios.get.mockRejectedValue(new Error('Server Error'));
+
+        const res = await request(app).get('/posts/1/comments');
+        expect(res.statusCode).toEqual(500);
+        expect(res.text).toEqual('Server Error');
+        expect(axios.get).toHaveBeenCalledWith(`${url}/1/comments`);
+        expect(axios.get).toHaveBeenCalledTimes(1);
     });
 });
 
+jest.mock('../../clients/httpClient');
+const token = keys.TOKEN;
+
 describe('POST /posts/:postID/comments', () => {
-    it('should create a comment under the post', async () => {
-        // params for tests
-        const postID = 1;
-        const commentData = {
-            post_id: postID,
-            name: 'Test',
-            email: 'test14156@gmail.com',
-            body: 'Test comment',
+    it('should create a comment', async () => {
+        const mockData = {
+            id: 1,
+            post_id: 1,
+            name: "Mr. Test",
+            email: "testtest1234@gmail.com",
+            body: "unit test for post"
         };
         const headers = {
             Authorization: `Bearer ${token}`,
             'Content-Type': 'application/json',
         };
+        createCommentRequest.mockResolvedValueOnce({ data: mockData });
 
-        const res = await axios.post(`${url}/${postID}/comments`, commentData, { headers });
-        expect(res.status).toEqual(201); // failed, and it's not good
-        // I receive here 200 status code instead of expected 201 statusCode
-        expect(res.data).toBeInstanceOf(Array);
+        const res = await request(app).post('/posts/1/comments').send(mockData);
+        expect(res.statusCode).toEqual(201);
+        expect(res.body).toEqual({data: mockData});
+        expect(createCommentRequest).toHaveBeenCalledTimes(1);
+        expect(createCommentRequest).toHaveBeenCalledWith(`${url}/1/comments`, JSON.stringify(mockData));
     });
 
-    it('should handle errors', async () => {
-        const postID = 'invalid';
-        try {
-            await axios.post(`${url}/${postID}/comments`);
-        } catch (error) {
-            expect(error.res.status).toEqual(500);
-            expect(error.res.data).toEqual('Server Error in POST request');
-        }
+    it('should return error if postID and post_id are not equal', async () => {
+        const mockData = { id: 1, post_id: 2, name: "Test name", email: "testtest@outlook.com", body: "unit test" };
+        const errorMessage = 'postID parameter and post_id field must be the same value';
+        createCommentRequest.mockRejectedValue({ message: errorMessage });
+      
+        const res = await request(app).post('/posts/1/comments').send(mockData);
+        expect(res.statusCode).toEqual(400);
+        expect(jest.fn()).not.toHaveBeenCalled();
+    });
+
+    it('should return error on server error', async () => {
+        const mockData = { id: 1, post_id: 1, name: "Test name", email: "testtest@outlook.com", body: "unit test" };
+        createCommentRequest.mockRejectedValue(new Error('Server Error'));
+
+        const res = await request(app).post('/posts/1/comments').send(mockData);
+        expect(res.statusCode).toEqual(500);
+        expect(res.text).toEqual('Server Error in POST request');
     });
 });
